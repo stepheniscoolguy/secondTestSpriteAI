ab29b6088cbd24655edaefbad8a55dad01689808
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-09-25 00:14:16 +0000

    adds new logic

---
+++ stringToBuffer.js
@@ -0,0 +1,10 @@
+// stringToBuffer.js
+
+/**
+ * Converts a string to a buffer.
+ * @param {string} str - The string to convert.
+ * @returns {Buffer} - The resulting buffer.
+ */
+export function stringToBuffer(str) {
+    return Buffer.from(str);
+  }
